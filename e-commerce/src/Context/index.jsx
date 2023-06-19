771e91105dd717dfd2ff7a7b6b00dd3caccf90f6
Juesgape <juesgape11@gmail.com>
2023-06-19 14:25:40 -0500

    Goblal context

---
+++ e-commerce/src/Context/index.jsx
@@ -0,0 +1,15 @@
+import { createContext } from 'react'
+
+const ShoppingCartContext = createContext()
+
+const ShoppingCartProvider = ({children}) => {
+    return (
+        <ShoppingCartContext.Provider>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+}
+
+export {
+    ShoppingCartProvider
+}
